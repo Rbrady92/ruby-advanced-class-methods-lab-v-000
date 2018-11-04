5f29e2eeb0477945bd373fb7f17bcd30eb47cc76
Ryan Brady <ryanbrady369@gmail.com>
2018-11-04 20:53:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,6 +42,10 @@ class Song
     false
   end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
   def self.new_from_filename(name)
 
   end
